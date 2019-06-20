fb841afc0827a927260a38261c8ea536604da49a
David Nguyen <tnguyen.david@gmail.com>
2019-06-19 21:32:20 -0700

    Set up AWS db

--- david-service/reservation/server/db/index.js
+++ david-service/reservation/server/db/index.js
@@ -4,8 +4,10 @@ const Promise = require('bluebird');
 const database = 'airbnb';
 
 const connection = mysql.createConnection({
-  user: 'root',
-  password: ''
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
 });
 
 const db = Promise.promisifyAll(connection);
